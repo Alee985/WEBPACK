559bea6c17dc736852bd9512b37f51325cbc026b
Alee985 <49112854+Alee985@users.noreply.github.com>
2021-07-30 23:59:59 +0500

    Changes made

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+import './style.css';
 import _ from 'lodash';
+import Icon from './pic.svg';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function component() {
-    const element = document.createElement('div');
+    //const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   // element.classList.add('hello');
+   // const myIcon = new Image();
+   // myIcon.src = Icon;
+  
+    //element.appendChild(myIcon);
+    
+    const element = document.createElement('button');
+    element.innerHTML = _.join(['Click', 'Here'], ' ');
+    element.classList.add('btn-primary');
+
+
+
+
   
     return element;
   }
